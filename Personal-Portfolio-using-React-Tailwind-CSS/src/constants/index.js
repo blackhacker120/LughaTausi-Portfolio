a836945689a05429703f91905904c414491f2429
The Black Hacker <162575571+blackhacker120@users.noreply.github.com>
2024-11-25 00:59:03 -0800

    Add files via upload

---
+++ Personal-Portfolio-using-React-Tailwind-CSS/src/constants/index.js
@@ -0,0 +1,56 @@
+import project3 from "../assets/images/e.png";
+
+
+export const HERO_CONTENT = `I am Christian Masako the Founder of Lugha Tausi I created this programming language using my own knowledge the main vision is to emphasize people to use the user friendly programming language and it is in form of Swahili that is easy to learn and understand
+`;
+
+export const ABOUT_TEXT = `I am Festo John Mchodo the Entrerpreneur and i have experience of data analysis 
+also i am on the part of the new programming language created by Christian Masako in this part i have 
+connected different people from different races and area to this platform.`; 
+
+export const ABOUT_ME = `I am John Makweba the FullStack Developer i have experience of web and mobile development
+and also i do the security and validation in this case i have a portion in the creation of the new programming language
+that was found by Christian Masako known as LughaTausi i used react , Node Js and Express Js and RestFul_API for 
+development`
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Senior Full Stack Developer",
+    company: "LughaTausi.inc",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  
+];
+
+export const PROJECTS = [
+  
+  
+  {
+    title: "LughaTausi Programming Language",
+    image:project3,
+    description:
+      "LughaTausi Swahili Programming Language that has RestFul API ,  Validation and Security and it is a user Friendly Programming Language its syntax are easy to learn and understand.",
+    technologies: ["Python" , "React Js" , "Next Js" , "RestFul API"],
+  },
+  
+];
+
+export const CONTACT =  
+    { 
+      address: "P.O.Box 443 , Dar Es Salaam , Tanzania",
+     phoneNo: "+255 769 298 518",
+     email: "christianmasako@gmail.com",
+    
+    };  
+
+export const DEVELOPERS =  
+   {
+  address: "P.O.Box 883 , Dodoma , Tanzania",
+  phoneNo: "+255 677 786 957",
+  email: "johnmakweba843@gmail.com",
+} 
+
+   
+
